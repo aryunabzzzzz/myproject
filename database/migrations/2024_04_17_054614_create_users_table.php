a445c7f579180d07efb728b7af13c379429c2b56
aryunabzzzzz <a11ab9@yandex.ru>
2024-04-25 16:10:42 +0700

    добавлена уникальность для nickname в таблице users

--- database/migrations/2024_04_17_054614_create_users_table.php
+++ database/migrations/2024_04_17_054614_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id')->primary();
-            $table->string('nickname');
+            $table->string('nickname')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('gender')->nullable();
