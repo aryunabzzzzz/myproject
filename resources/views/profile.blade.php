829c9ae761e1b344c521a02b24fa2b239bbf2b28
aryunabzzzzz <a11ab9@yandex.ru>
2024-05-15 11:59:03 +0700

    добавлен пустой аватар по дефолту

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,7 +9,11 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <img src="{{$user->img_url}}" width="300" height="300">
+                @if($user->img_url)
+                    <img src="{{$user->img_url}}" width="300" height="300">
+                @else
+                    <img src="https://miramirov.gosuslugi.ru/netcat_files/11/143/no_foto_1.png" width="300" height="300">
+                @endif
             </div>
 
             <div class="col">
